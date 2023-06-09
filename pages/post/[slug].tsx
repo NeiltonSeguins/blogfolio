c12e2a6a03ebfe70dd13f119cac13d0a61dcb571
NeiltonSeguins <seguins.neilton@gmail.com>
2023-06-09 17:06:57 -0300

    Esliliza a página de post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -6,11 +6,48 @@ import {
   getPublishedBlogPosts,
   getSingleBlogPost,
 } from "../../services/notion-service";
+import {
+  Button,
+  Footer,
+  Menu,
+  Profile,
+  Sidebar,
+  SocialNetwork,
+} from "../../components";
 
 const BlogPost = ({
   post,
   markdown,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const components = {
+    h1: ({ children }: any) => (
+      <h1 className="text-5xl font-poppins font-bold text-primary mb-11 hover:text-hover">
+        # {children}
+      </h1>
+    ),
+    h2: ({ children }: any) => (
+      <h2 className="text-3xl font-poppins font-bold text-primary my-6">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }: any) => (
+      <h3 className="text-2xl font-poppins font-bold text-primary my-6">
+        {children}
+      </h3>
+    ),
+    p: ({ children }: any) => (
+      <p className="text-lg font-poppins text-secondary text-justify my-6">
+        {children}
+      </p>
+    ),
+    ul: ({ children }: any) => (
+      <ul className="list-disc text-secondary text-lg font-poppins">{children}</ul>
+    ),
+    li: ({ children }: any) => (
+      <li className=" my-4">{children}</li>
+    ),
+  };
+
   return (
     <>
       <Head>
@@ -28,10 +65,36 @@ const BlogPost = ({
         />
         <meta name="og:image" title="og:image" content={post.cover} />
       </Head>
-      <div className="min-h-screen">
-        <article className="prose">
-          <ReactMarkdown>{markdown}</ReactMarkdown>
-        </article>
+      <div className="h-screen flex flex-1">
+        <div className="flex flex-col items-center justify-between w-1/4 border-r-[1px] border-primary">
+          <Sidebar className="h-2/3 p-8 flex flex-col items-center justify-between">
+            <div className="flex flex-col items-center text-center">
+              <Profile />
+              <SocialNetwork />
+            </div>
+            <Menu />
+          </Sidebar>
+          <Footer />
+        </div>
+        <main className="w-full flex justify-between">
+          <div className="p-8 w-full flex flex-col items-center pr-12 pl-12">
+            <article className="my-0 mx-auto w-4/5 overflow-auto">
+              <div
+                className="rounded-md mb-11 bg-cover bg-center h-[280px] w-full"
+                style={{
+                  backgroundImage: `url(${post.cover})`,
+                }}
+              ></div>
+              <div className="markdown-img:w-3/4 markdown-img:h-[450px] markdown-img:mx-auto markdown-img:rounded-lg">
+                <ReactMarkdown children={markdown} components={components} />
+              </div>
+            </article>
+          </div>
+          <Sidebar className="py-8 px-8 pt-10 pb-10  w-20 flex flex-col justify-between items-center border-l-[1px] border-primary">
+            <Button tipo="theme" />
+            <Button tipo="scrollUp" />
+          </Sidebar>
+        </main>
       </div>
     </>
   );
